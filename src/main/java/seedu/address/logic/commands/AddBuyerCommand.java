197270e3cec739ecffe5849aa975089e93fa0e03
peasantbird <bertrand.wong.xh@gmail.com>
2023-10-16 16:59:26 +0800

    Create add-buyer feature

---
+++ src/main/java/seedu/address/logic/commands/AddBuyerCommand.java
@@ -0,0 +1,82 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.buyer.Buyer;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+/**
+ * Adds a buyer to the address book.
+ */
+public class AddBuyerCommand extends Command {
+
+    public static final String COMMAND_WORD = "buyer";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a buyer to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + PREFIX_INFO + "INFO "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_INFO + "Central Area 5 Room Condominium "
+            + PREFIX_TAG + "friends "
+            + PREFIX_TAG + "owesMoney";
+
+    public static final String MESSAGE_SUCCESS = "New buyer added: %1$s";
+    public static final String MESSAGE_DUPLICATE_BUYER = "This buyer already exists in the address book";
+
+    private final Buyer toAdd;
+
+    /**
+     * Creates an AddBuyerCommand to add the specified {@code Buyer}
+     */
+    public AddBuyerCommand(Buyer buyer) {
+        requireNonNull(buyer);
+        toAdd = buyer;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasPerson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_BUYER);
+        }
+
+        model.addPerson(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddBuyerCommand)) {
+            return false;
+        }
+
+        AddBuyerCommand otherAddCommand = (AddBuyerCommand) other;
+        return toAdd.equals(otherAddCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
+    }
+}
