2d4907828d4dbdeea0a66d6f9b35c10bde28ca7a
iantsaii <ianfourr@gmail.com>
2023-10-16 22:46:45 +0800

    Add view sellers command

---
+++ src/main/java/seedu/address/logic/commands/ListBuyersCommand.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_BUYERS;
+
+import seedu.address.model.Model;
+
+/**
+ * List all Buyers in the address book to the user.
+ */
+public class ListBuyersCommand extends Command {
+
+    public static final String COMMAND_WORD = "list-b";
+    public static final String MESSAGE_SUCCESS = "Listed all buyers";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_BUYERS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+}
