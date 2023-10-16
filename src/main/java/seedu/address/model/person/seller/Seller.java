b2cf05bc604894cd2925678513c499e77e1dd9f1
J-hta-n <e0969121@u.nus.edu>
2023-10-16 14:27:34 +0800

    Fix checkstyle issues for Seller

--- src/main/java/seedu/address/model/person/seller/Seller.java
+++ src/main/java/seedu/address/model/person/seller/Seller.java
@@ -1,15 +1,31 @@
 package seedu.address.model.person.seller;
 
-import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Set;
 
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+
+
+/**
+ * Represents a Seller in the address book.
+ */
 public class Seller extends Person {
-    Address sellingAddress;
-    HouseInfo houseInfo;
-    public Seller(Name name, Phone phone, Email email, Address address, Address sellingAddress, HouseInfo houseInfo, Set<Tag> tags) {
+    private final Address sellingAddress;
+    private final HouseInfo houseInfo;
+    /**
+     * Every field must be present and not null (super class does these checks too)
+     */
+    public Seller(Name name, Phone phone, Email email, Address address, Address sellingAddress,
+                  HouseInfo houseInfo, Set<Tag> tags) {
         super(name, phone, email, address, tags);
+        requireAllNonNull(sellingAddress, houseInfo);
         this.sellingAddress = sellingAddress;
         this.houseInfo = houseInfo;
     }
