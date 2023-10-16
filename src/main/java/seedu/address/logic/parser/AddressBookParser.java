1200fcec433940ecedea888e2af29cfd20ca3b09
iantsaii <ianfourr@gmail.com>
2023-10-16 22:48:47 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,8 +17,8 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListBuyersCommand;
+import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListSellersCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
