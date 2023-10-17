214cd1ac2424ecbf95216efc02ed353a21344c37
ruiyangzh <zhao.ruiyang90@gmail.com>
2023-10-18 00:15:29 +0800

    Add deletion commands

---
+++ src/main/java/seedu/address/logic/commands/DeleteSellersCommand.java
@@ -0,0 +1,70 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.seller.Seller;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Deletes a seller identified using it's displayed index from the address book.
+ */
+public class DeleteSellersCommand extends Command {
+
+    public static final String COMMAND_WORD = "delete-s";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the seller identified by the index number used in the displayed seller list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_SELLER_SUCCESS = "Deleted Seller: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteSellersCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Seller> lastShownList = model.getFilteredSellerList();
+        //TODO add these methods to model.
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Seller sellerToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteSeller(sellerToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_SELLER_SUCCESS, Messages.format(sellerToDelete)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteSellersCommand)) {
+            return false;
+        }
+
+        DeleteSellersCommand otherDeleteSellersCommand = (DeleteSellersCommand) other;
+        return targetIndex.equals(otherDeleteSellersCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,19 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.commands.AddBuyerCommand;
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddSellerCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListBuyersCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.ListSellersCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -93,6 +81,12 @@ public class AddressBookParser {
         case ListBuyersCommand.COMMAND_WORD:
             return new ListBuyersCommand();
 
+        case DeleteBuyerCommand.COMMAND_WORD:
+            return new DeleteBuyerCommandParser().parse(arguments);
+
+        case DeleteSellersCommand.COMMAND_WORD:
+            return new DeleteSellerCommandParser().parse(arguments);
+            
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
