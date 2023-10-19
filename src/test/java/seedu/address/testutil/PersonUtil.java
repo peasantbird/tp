6a6d29ff95d21a791600f4638099e600ef337169
ruiyangzh <zhao.ruiyang90@gmail.com>
2023-10-19 22:44:59 +0800

    Fix parser test

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -2,34 +2,63 @@ package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INFO;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SELLING_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import seedu.address.logic.commands.AddBuyerCommand;
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddSellerCommand;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.buyer.Buyer;
+import seedu.address.model.person.seller.Seller;
+
 /**
  * A utility class for Person.
  */
 public class PersonUtil {
-
     /**
-     * Returns an add command string for adding the {@code person}.
+     * Returns an add command string for adding the {@code buyer}.
      */
-    public static String getAddCommand(Person person) {
-        return AddCommand.COMMAND_WORD + " " + getPersonDetails(person);
+    public static String getAddCommand(Buyer buyer) {
+        return AddBuyerCommand.COMMAND_WORD + " " + getBuyerDetails(buyer);
+    }
+    /**
+     * Returns an add command string for adding the {@code seller}.
+     */
+    public static String getAddCommand(Seller seller) {
+        return AddSellerCommand.COMMAND_WORD + " " + getSellerDetails(seller);
     }
 
     /**
-     * Returns the part of command string for the given {@code person}'s details.
+     * Returns the part of command string for the given {@code buyer}'s details.
      */
-    public static String getPersonDetails(Person person) {
+    public static String getBuyerDetails(Buyer buyer) {
         StringBuilder sb = new StringBuilder();
-        sb.append(PREFIX_NAME + person.getName().fullName + " ");
-        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
-        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
-        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        person.getTags().stream().forEach(
+        sb.append(PREFIX_NAME + buyer.getName().fullName + " ");
+        sb.append(PREFIX_PHONE + buyer.getPhone().value + " ");
+        sb.append(PREFIX_EMAIL + buyer.getEmail().value + " ");
+        sb.append(PREFIX_ADDRESS + buyer.getAddress().value + " ");
+        sb.append(PREFIX_INFO + buyer.getBuyHouseInfo().toString() + " ");
+        buyer.getTags().stream().forEach(
+                s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+        return sb.toString();
+    }
+    /**
+     * Returns the part of command string for the given {@code seller}'s details.
+     */
+    public static String getSellerDetails(Seller seller) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + seller.getName().fullName + " ");
+        sb.append(PREFIX_PHONE + seller.getPhone().value + " ");
+        sb.append(PREFIX_EMAIL + seller.getEmail().value + " ");
+        sb.append(PREFIX_ADDRESS + seller.getAddress().value + " ");
+        sb.append(PREFIX_SELLING_ADDRESS + seller.getSellingAddress().value + " ");
+        sb.append(PREFIX_INFO + seller.getSellHouseInfo().toString() + " ");
+        seller.getTags().stream().forEach(
                 s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
         return sb.toString();
