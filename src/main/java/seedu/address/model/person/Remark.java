5ee9d0e011bf9573dc9173016f852e1e2957cf1c
peasantbird <bertrand.wong.xh@gmail.com>
2023-10-20 19:09:35 +0800

    Modify commands, storage and test to use updated Person

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -3,11 +3,27 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 
 public class Remark {
+
+    public static final String MESSAGE_CONSTRAINTS = "Remark can take any values, and it should not be blank";
+
+    /*
+     * The first character of the remark must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
     public final String value;
 
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
+    }
+
+    /**
+     * Returns true if a given string is a valid remark.
+     */
+    public static boolean isValidRemark(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
