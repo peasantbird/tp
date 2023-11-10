02efc519405f2dda1d71c0ed7aa1f377ec36b0ff
ruiyangzh <zhao.ruiyang90@gmail.com>
2023-11-10 09:29:39 +0800

    Enhance CommandWarningsTest

--- src/test/java/seedu/address/logic/CommandWarningsTest.java
+++ src/test/java/seedu/address/logic/CommandWarningsTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -32,9 +34,11 @@ public class CommandWarningsTest {
         assertTrue(warnings.containsWarningString(BLANK));
         assertTrue(warnings.containsWarningString(BLANK2));
     }
+
     @Test
     public void warnings_getWarningMessage() {
         CommandWarnings warnings = new CommandWarnings();
+        assertEquals(warnings.getWarningMessage(), "");
         warnings.addWarning(TEST1);
         assertEquals(warnings.getWarningMessage(),
                 "Warning!; [" + TEST1 + "]\nPlease ignore if this is expected.");
@@ -43,12 +47,74 @@ public class CommandWarningsTest {
                 "Warning!; [" + TEST1 + ", " + TEST4 + "]\nPlease ignore if this is expected.");
     }
     @Test
+    public void warnings_toString() {
+        CommandWarnings warnings = new CommandWarnings();
+        assertEquals(warnings.toString(), "[]");
+        warnings.addWarning(TEST1);
+        assertEquals(warnings.toString(),
+                "[" + TEST1 + "]");
+        warnings.addWarning(TEST4);
+        assertEquals(warnings.toString(),
+                "[" + TEST1 + ", " + TEST4 + "]");
+    }
+
+    @Test
+    public void warnings_contains() {
+        CommandWarnings warnings = new CommandWarnings();
+        assertFalse(warnings.containsWarnings());
+        assertFalse(warnings.containsWarningString(""));
+        assertFalse(warnings.containsWarningString("   "));
+
+        warnings.addWarning(TEST4);
+        assertTrue(warnings.containsWarnings());
+        assertTrue(warnings.containsWarningString(TEST4));
+        assertFalse(warnings.containsWarningString(TEST4.substring(4)));
+
+        warnings.addWarning(TEST2);
+        assertTrue(warnings.containsWarningString(TEST2));
+        assertFalse(warnings.containsWarningString(TEST2.trim()));
+    }
+
+    @Test
     public void warnings_equality() {
         CommandWarnings warnings = new CommandWarnings();
         warnings.addWarning("Test");
+        warnings.addWarning("Test2");
+
+        CommandWarnings warningsCopy = new CommandWarnings();
+        warningsCopy.addWarning("Test");
+        warningsCopy.addWarning("Test2");
+
+        CommandWarnings warningsInvertedOrder = new CommandWarnings();
+        warningsInvertedOrder.addWarning("Test2");
+        warningsInvertedOrder.addWarning("Test");
+
+        CommandWarnings warningsDuplicated = new CommandWarnings();
+        warningsDuplicated.addWarning("Test");
+        warningsDuplicated.addWarning("Test2");
+        warningsDuplicated.addWarning("Test2");
+        warningsDuplicated.addWarning("Test");
+
         CommandWarnings other = new CommandWarnings();
-        assertFalse(warnings.equals(other));
-        other.addWarning("Test");
-        assertTrue(warnings.equals(other));
+        warningsCopy.addWarning("Test2");
+
+        CommandWarnings blank = new CommandWarnings();
+
+        CommandWarnings empty = new CommandWarnings();
+        empty.addWarning("");
+
+        CommandWarnings nullWarnings = null;
+
+        assertEquals(warnings, warnings);
+        assertNotEquals(warnings, other);
+        assertEquals(warnings, warningsCopy);
+        assertEquals(warnings, (Object) warningsCopy);
+        assertEquals(blank, new CommandWarnings());
+        assertNotEquals(empty, blank);
+        assertNotEquals(empty, null);
+        assertNull(nullWarnings);
+        assertEquals(warningsInvertedOrder, warningsCopy);
+        assertEquals(warnings, warningsInvertedOrder);
+        assertEquals(warnings, warningsDuplicated);
     }
 }
